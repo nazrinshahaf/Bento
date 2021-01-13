77f46a69e28eec296bbcd159e248bbd36d947ed2
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2021-01-13 21:47:19 +0800

    changed alot

--- js/time.js
+++ js/time.js
@@ -1,18 +1,18 @@
 window.onload = displayClock();
 function displayClock() {
   const monthNames = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
   ];
 
   // Set to true to use a 12 hour date format
@@ -21,22 +21,27 @@ function displayClock() {
   var d = new Date();
   var mm = monthNames[d.getMonth()];
   var dd = d.getDate();
-  var min = (mins = ('0' + d.getMinutes()).slice(-2));
+  var min = (mins = ("0" + d.getMinutes()).slice(-2));
   var hh = d.getHours();
-  var ampm = '';
+  var ss = d.getSeconds();
+  var ampm = "";
 
   if (format_12hour) {
-    ampm = hh >= 12 ? ' pm' : ' am';
+    ampm = hh >= 12 ? " pm" : " am";
     hh = hh % 12;
     hh = hh ? hh : 12; //show mod 0 as 12
   }
 
-  document.getElementById('hour').innerText = hh;
-  document.getElementById('separator').innerHTML = ' : ';
-  document.getElementById('minutes').innerText = min + ampm;
+  if (ss < 10) {
+    ss = "0" + ss;
+  }
 
-  document.getElementById('month').innerText = mm;
-  document.getElementById('day').innerText = dd;
+  document.getElementById("hour").innerText = hh;
+  document.getElementById("minutes").innerText = min + ampm;
+  document.getElementById("seconds").innerText = ss;
+
+  document.getElementById("month").innerText = mm;
+  document.getElementById("day").innerText = dd;
 
   setTimeout(displayClock, 1000);
 }
